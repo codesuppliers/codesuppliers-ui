af3b417ffb1b7b78396a146f61e9710a1ed58aca
kemoth00 <tom.tailor2017@gmail.com>
2022-09-12 23:44:34 +0200

    dayend

--- resources/js/components/layout/Header.vue
+++ resources/js/components/layout/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <header class="flex my-4 divide-x divide-gray-600 space-x-4">
-    <router-link to="/" class="flex items-center">
+    <router-link to="/" class="flex justify-center items-center">
       <SvgIcon name="IconLogo" />
       <span class="text-2xl font-bold">Code</span>
       <span class="text-2xl font-bold text-sky-500">Suppliers</span>
-      <div
-        class="flex justify-center items-center rounded-lg border border-slate-300 ml-2 w-6 h-6 text-sm"
+      <span
+        class="flex justify-center items-center rounded-lg border border-slate-300 ml-2 text-xs aspect-square w-6"
       >
-        <span>UI</span>
-      </div>
+        UI
+      </span>
     </router-link>
     <div></div>
   </header>
